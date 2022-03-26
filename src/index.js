9435e23c100ba339fb0a8937f1e02eb355c99f5c
Pablo Padilla <pablopadillamaldonado@gmail.com>
2022-03-26 16:08:29 -0600

    Deploy to Heroku

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Heroku
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.js'));
+ });
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
+ // End Heroku
+
 /*class Square extends React.Component {
     constructor(props) {
         super(props);
